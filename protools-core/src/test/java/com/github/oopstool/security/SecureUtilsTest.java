06bfd29260c48ec871cf098058a026cb19cc185e
oopstools <houguangyu@163.com>
2021-03-30 17:06:07 +0800

    新增p12文件读取

--- protools-core/src/test/java/com/github/oopstool/security/SecureUtilsTest.java
+++ protools-core/src/test/java/com/github/oopstool/security/SecureUtilsTest.java
@@ -1,6 +1,15 @@
 package com.github.oopstool.security;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+
+import com.github.oopstool.string.StringUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SecureUtilsTest {
@@ -40,4 +49,60 @@ public class SecureUtilsTest {
         String encryptString1 = SecureUtils.encryptBySHA256("111111","sdae");
         assert encryptString1.length()==64;
     }
+
+    @Test
+    public void generateKeyPair() throws Exception {
+        KeyPair keyPair = SecureUtils.generateKeyPair();
+        Assert.assertNotNull(keyPair);
+        Assert.assertNotNull(keyPair.getPrivate());
+        Assert.assertNotNull(keyPair.getPublic());
+    }
+
+    @Test
+    public void keyForEncodedBase64() throws Exception {
+        KeyPair keyPair = SecureUtils.generateKeyPair();
+        String pri = SecureUtils.keyForEncodedBase64(keyPair.getPrivate());
+        String pub = SecureUtils.keyForEncodedBase64(keyPair.getPublic());
+        Assert.assertNotNull(pri);
+        Assert.assertNotNull(pub);
+
+
+    }
+
+    @Test
+    public void getPublicKey() throws Exception {
+        KeyPair keyPair = SecureUtils.generateKeyPair();
+        String pri = SecureUtils.keyForEncodedBase64(keyPair.getPrivate());
+        String pub = SecureUtils.keyForEncodedBase64(keyPair.getPublic());
+        PublicKey publicKey = SecureUtils.getPublicKey(pub);
+        Assert.assertNotNull(publicKey);
+    }
+
+    @Test
+    public void getPrivateKey() throws Exception {
+        KeyPair keyPair = SecureUtils.generateKeyPair();
+        String pri = SecureUtils.keyForEncodedBase64(keyPair.getPrivate());
+        String pub = SecureUtils.keyForEncodedBase64(keyPair.getPublic());
+        PrivateKey privateKey = SecureUtils.getPrivateKey(pri);
+        Assert.assertNotNull(privateKey);
+    }
+
+    @Test
+    public void encrypt() throws Exception {
+        KeyPair keyPair = SecureUtils.generateKeyPair();
+        byte[] helloWords = SecureUtils.encrypt(StringUtils.bytes("helloWord"), keyPair.getPublic());
+        System.out.println(StringUtils.str(helloWords, StandardCharsets.UTF_8));
+        Assert.assertNotNull(helloWords);
+        byte[] decrypt = SecureUtils.decrypt(helloWords, keyPair.getPrivate());
+        String str = StringUtils.str(decrypt, StandardCharsets.UTF_8);
+        System.out.println(StringUtils.str(decrypt, StandardCharsets.UTF_8));
+        assert "helloWord".equals(str);
+    }
+
+
+
+    @Test
+    public void readP12Cert() {
+    }
+
 }
